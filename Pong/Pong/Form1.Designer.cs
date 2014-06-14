1ea9845d1672c0d5bf23cd0db850482f9dc84381
Vergas <kevin@fuks.net>
2014-06-14 15:42:18 +0200

    plein ecran et commentaires

--- Pong/Pong/Form1.Designer.cs
+++ Pong/Pong/Form1.Designer.cs
@@ -30,16 +30,20 @@
         {
             this.components = new System.ComponentModel.Container();
             this.espaceJeu = new System.Windows.Forms.Panel();
-            this.raquette = new System.Windows.Forms.PictureBox();
             this.balle = new System.Windows.Forms.PictureBox();
+            this.raquette = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
             this.espaceJeu.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.raquette)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.balle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.raquette)).BeginInit();
             this.SuspendLayout();
             // 
             // espaceJeu
             // 
+            this.espaceJeu.Controls.Add(this.scoreLabel);
+            this.espaceJeu.Controls.Add(this.label);
             this.espaceJeu.Controls.Add(this.balle);
             this.espaceJeu.Controls.Add(this.raquette);
             this.espaceJeu.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -47,15 +51,6 @@
             this.espaceJeu.Name = "espaceJeu";
             this.espaceJeu.Size = new System.Drawing.Size(735, 570);
             this.espaceJeu.TabIndex = 0;
-            // 
-            // raquette
-            // 
-            this.raquette.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.raquette.Location = new System.Drawing.Point(273, 517);
-            this.raquette.Name = "raquette";
-            this.raquette.Size = new System.Drawing.Size(175, 20);
-            this.raquette.TabIndex = 0;
-            this.raquette.TabStop = false;
             // 
             // balle
             // 
@@ -66,9 +61,36 @@
             this.balle.TabIndex = 1;
             this.balle.TabStop = false;
             // 
+            // raquette
+            // 
+            this.raquette.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.raquette.Location = new System.Drawing.Point(273, 517);
+            this.raquette.Name = "raquette";
+            this.raquette.Size = new System.Drawing.Size(175, 20);
+            this.raquette.TabIndex = 0;
+            this.raquette.TabStop = false;
+            // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(12, 23);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(41, 13);
+            this.label.TabIndex = 2;
+            this.label.Text = "Score :";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(59, 23);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(13, 13);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Text = "0";
             // 
             // Form1
             // 
@@ -78,10 +100,10 @@
             this.Controls.Add(this.espaceJeu);
             this.Name = "Form1";
             this.Text = "Pong";
-            
             this.espaceJeu.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.raquette)).EndInit();
+            this.espaceJeu.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.balle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.raquette)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -92,6 +114,8 @@
         private System.Windows.Forms.PictureBox balle;
         private System.Windows.Forms.PictureBox raquette;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label label;
     }
 }
 
--- Pong/Pong/Form1.cs
+++ Pong/Pong/Form1.cs
@@ -11,32 +11,39 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        public int vitesse = 1;
+        // initialisation des paramètres
+        public int vitesse = 6;
         public int gaucheDroite = 1;
         public int hautBas = 1;
         public int score = 0;
+
+
         public Form1()
         {
             InitializeComponent();
             timer1.Enabled = true;
-
-            raquette.Top = (espaceJeu.Bottom - 40);
+            this.FormBorderStyle = FormBorderStyle.None; // pas de barre en haut
+            this.Bounds = Screen.PrimaryScreen.Bounds; // plein écran
+            raquette.Top = (espaceJeu.Bottom - 40);  // positionne la raquette en hauteur
 
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            raquette.Left = Cursor.Position.X - (raquette.Width / 2);
+            raquette.Left = Cursor.Position.X - (raquette.Width / 2); // la raquette suit la souris 
+
             balle.Left = balle.Left + vitesse*gaucheDroite;
             balle.Top = balle.Top + vitesse*hautBas;
 
-            if (balle.Bottom >= raquette.Top && balle.Bottom <= raquette.Bottom && balle.Left >= raquette.Left && balle.Right <= raquette.Right)
+            if (balle.Bottom >= raquette.Top && balle.Bottom <= raquette.Bottom && balle.Left >= raquette.Left && balle.Right <= raquette.Right) // si la balle touche la raquette : 
             {
                 vitesse = vitesse + 2;
-                hautBas = -1;
+                hautBas = -1; // balle change de sens verticalement et remonte
                 score = score + 1;
+                scoreLabel.Text = score.ToString();
             }
 
+            // changement de sens horizontal quand la balle touche bord gauche ou droite
             if (balle.Left <= espaceJeu.Left)
             {
                 gaucheDroite = 1;
@@ -45,17 +52,20 @@ namespace WindowsFormsApplication1
             {
                 gaucheDroite = -1;
             }
+            // changement de sens vertical quand la balle touche le sommet de l'espace de jeu
             if (balle.Top <= espaceJeu.Top)
             {
                 hautBas = 1;
             }
 
+            // fin du jeu quand la balle arrive en bas de l'espace de jeu
             if (balle.Bottom >= espaceJeu.Bottom)
             {
                 timer1.Enabled = false;
             }
 
         }
+
 
     }
 }
